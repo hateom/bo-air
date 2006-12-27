4fe1f5ab5fad2eb1e674899970f80e42237d97cf
tomasz.huczek <tomasz.huczek@f45e810e-8225-0410-8430-f7d63d91aa8e>
2006-12-27 14:22:13 +0000

    Kilka dodatkowych plikow, i klas ulatwiajacych pisanie tekstowej aplikacji.

--- processor/src/p_main.cpp
+++ processor/src/p_main.cpp
@@ -1,68 +1,44 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdarg.h>
+#include "p_output.h"
+#include "p_utils.h"
 #include "p_parser.h"
-
-#define COL_RED "\e[31m"
-#define COL_BLU "\e[36m"
-#define COL_GRN "\e[32m"
-#define COL_DEF "\e[37m"
-
-static int silent = 0;
-
-bool file_exists( const char * filename )
-{
-    FILE * file;
-
-    file = fopen( filename, "r" );
-    if( !file ) return( false );
-    fclose( file );
-    return( true );
-}
-
-void prints( const char * str, ... )
-{
-    if( silent ) return;
-    char buffer[512];
-    va_list al;
-    va_start( al, str );
-    vsprintf( buffer, str, al );
-    va_end( al );
-
-    printf( "%s", buffer );
-}
 
 int main( int argc, char * argv[] )
 {
     int file_in = 1;
+    bool flag;
 
     if( argc == 3 && strcmp( argv[1], "-s" ) == 0 )
     {
-        silent = 1;
+        pOut->set_silent( true );
         file_in = 2;
     }
 
-    prints( "%s", COL_DEF );
+    pOut->print( "%s", COL_DEF );
     if( argc == 1 || argc > 3 )
     {
-        prints( "!!! %sERROR%s> usage:\t%s (-s) <input_file>\n\t-s\t silent mode\n\n", COL_RED, COL_DEF, argv[0] );
+        pOut->print( "!!! %sERROR%s> wrong parameters\nusage:\n\t%s (-s) <input_file>\nparams:\n\t-s\t silent mode\n\n", COL_RED, COL_DEF, argv[0] );
         return( -1 );
     }
 
-    prints( ">>> using input file <%s>... ", argv[file_in] );
-    if( !file_exists( argv[file_in] ) )
+    pOut->print( "%s>>> %susing input file <%s%s%s>... ", COL_BLU, COL_GRY, COL_DEF, COL_GRY, argv[file_in] );
+    if( !p_utils::file_exists( argv[file_in] ) )
     {
-        prints( "[ %s!!%s ]\n!!! ERROR> file <%s> does not exists!\n", COL_RED, COL_DEF, argv[file_in] );
+        pOut->print( "%s[ %s!!%s ]\n!!! ERROR> file <%s> does not exists!\n", COL_GRY, COL_RED, COL_GRY, argv[file_in] );
         return( -2 );
     }
     else
     {
-        prints( "[ %sOK%s ]\n", COL_GRN, COL_DEF );
+        pOut->print( "%s[ %sOK%s ]\n", COL_GRY, COL_GRN, COL_GRY );
     }
 
-    prints( ">>> processing input file and generating output.\n" );
-    Parser->process( argv[file_in] );
+    pOut->print( "%s>>>%s processing input file and generating output.\n", COL_BLU, COL_GRY );
+    flag = Parser->process( argv[file_in] );
     Parser->release();
+
+    pOut->print( "\n%s>>> %sprocessing is finished [ %s%s%s ]\n", COL_BLU, COL_GRY, flag?COL_GRN:COL_RED, flag?"OK":"!!", COL_GRY );
+    pOut->print( "%s>>> %scontact us: %stomasz.huczek@gmail.com%s, %sjasinski.andrzej@gmail.com%s.\n\n", COL_BLU, COL_GRY, COL_DEF, COL_GRY, COL_DEF, COL_GRY );
 
     return( 0 );
 }
