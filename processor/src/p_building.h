641f01617273fc5afa439c1c3612ff82c16face2
tomasz.huczek <tomasz.huczek@f45e810e-8225-0410-8430-f7d63d91aa8e>
2006-12-30 00:00:22 +0000

    Kilka nowych klas reprezentujacych typy danych (antena aka transmitter, oraz budynek aka building)

---
+++ processor/src/p_building.h
@@ -0,0 +1,23 @@
+#ifndef __P_BUILDING_H__
+#define __P_BUILDING_H__
+
+class pBuilding
+{
+public:
+    pBuilding();
+    pBuilding( int x, int y, int population );
+    ~pBuilding();
+
+    int get_x() const;
+    int get_y() const;
+    int get_population() const;
+
+    void set_population( int pop );
+    void set_pos( int x, int y );
+
+protected:
+    int x, y, population;
+};
+
+#endif // __P_BUILDING_H__
+
