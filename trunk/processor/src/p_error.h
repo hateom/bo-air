b6fb5f3e64670e54198778a65c3e2c90c88c7387
tomasz.huczek <tomasz.huczek@f45e810e-8225-0410-8430-f7d63d91aa8e>
2007-01-06 11:53:19 +0000

    Dodanie kilku plikow, o ktorych zapomnialem poprzednio

---
+++ trunk/processor/src/p_error.h
@@ -0,0 +1,30 @@
+#ifndef __P_ERROR_H__
+#define __P_ERROR_H__
+
+#ifdef P_DEBUG
+#define P_ASSERT( MSG, EXP ) if( !(EXP) ) { throw pException( MSG, #EXP, __FILE__, __LINE__ ); }
+#else
+#define P_ASSERT( MSG, EXP )
+#endif
+
+#include <string>
+
+class pException
+{
+public:
+    pException( const char * ms, const char * ex, const char * fil, int lin ) : 
+        line( lin ), msg( ms ), exp( ex ), file( fil ) {}
+    ~pException() {}
+    const char * show()
+    {
+        static char buffer[256] = "";
+        sprintf( buffer, "%s at %d> `%s` : %s", file.c_str(), line, exp.c_str(), msg.c_str() );
+        return( buffer );
+    }
+private:
+    int line;
+    std::string msg, exp, file;
+};
+
+#endif // __P_ERROR_H__
+
---
+++ trunk/processor/src/p_types.h
@@ -0,0 +1,8 @@
+#ifndef __P_TYPES_H__
+#define __P_TYPES_H__
+
+#include "p_transmitter.h"
+#include "p_building.h"
+
+#endif // __P_TYPES_H__
+
