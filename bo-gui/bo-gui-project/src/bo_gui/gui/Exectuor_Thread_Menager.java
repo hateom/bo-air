87edaa6c86fbd75a0cd7bf81bdcfe8f16e9ccf33
jasinski.andrzej <jasinski.andrzej@f45e810e-8225-0410-8430-f7d63d91aa8e>
2007-01-20 22:12:29 +0000

    * rysowanie wykresu * watki zdaja sie dzialac dobrze * TODO: usunac "osierocanie" obiektow

--- bo-gui/bo-gui-project/src/bo_gui/gui/Exectuor_Thread_Menager.java
+++ bo-gui/bo-gui-project/src/bo_gui/gui/Exectuor_Thread_Menager.java
@@ -1,28 +1,93 @@
 package bo_gui.gui;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.swing.SwingUtilities;
+
 import bo_gui.executor.ProcessorExecutor;
+import bo_gui.executor.SolutionStruct;
 
 public class Exectuor_Thread_Menager {
 	
-	private Thread exec;
+	private Thread exec = null;
 	private MainWindow okno;
+	private List<Float> results_list;
+	private int best_solution;
+	private boolean halted = false;
+	private Process proces;
 	
 	public Exectuor_Thread_Menager( MainWindow okno){
 		this.okno = okno;
+		results_list = new ArrayList<Float>();
 	}
 	
 	
-	public void start_thread(){
-		okno.text_pane.setText("jhuhu");
-		exec = new Thread( new ProcessorExecutor(this) );
-		exec.start();
-	}
-	
-	public void terminate_thread(){
+	public synchronized void start_thread(){
+		if (exec!=null && exec.isAlive()){
+			terminate_thread();
+			proces.destroy();
+			halted = true;
+			okno.StartButton.setEnabled(false);
+		}
+		else{
+			halted = false;
+			Calendar datownik = Calendar.getInstance();
+			print_debug_info("-----" + datownik.getTime().toString()+"-----");
+			print_debug_info("[M]Starting processing thread...");
+			exec = new Thread( new ProcessorExecutor(this, "input_data") );
+			exec.start();
+			okno.StartButton.setText("Stop");
+		}
 		
 	}
 	
-	public void zrobcos( String text){
-		okno.text_pane.setText(okno.text_pane.getText()+"\n"+text);
+	public synchronized void signalizeFinish(){
+		okno.StartButton.setEnabled(true);
+		okno.StartButton.setText("Start");
 	}
+	
+	public synchronized void setProcess( Process proc){
+		proces = proc;
+	}
+	
+	public synchronized void terminate_thread(){
+		print_debug_info("[M]Requesting process termination...");
+		
+		exec.interrupt();
+	}
+	
+	public synchronized void setProcessResult( List<SolutionStruct> solution ){
+		if (halted){
+			results_list.clear();
+		}else{
+			results_list.clear();
+			float max_val = 0.0f;
+			
+			for (int i=0; i<solution.size();i++ ){
+				results_list.add(solution.get(i).profit);
+				if (solution.get(i).profit > max_val) {
+					best_solution = i;
+					max_val = solution.get(i).profit;
+				}
+			}
+			okno.graph.setPoints(results_list);
+		}
+		okno.StartButton.setEnabled(true);
+	}
+	
+	public int getMaxVal(){
+		return best_solution;
+	}
+	// taka struktura zeby uniknac bledow  Interrupted attempt to aquire write lock..
+	public void print_debug_info( final String text ) {
+	        Runnable setPaneText = new Runnable() {
+	            public void run() {
+	            	okno.text_pane.setText(okno.text_pane.getText()+text+"\n");
+	        		okno.text_pane.setCaretPosition(okno.text_pane.getDocument().getLength());
+	            }
+	        };
+	        SwingUtilities.invokeLater(setPaneText);
+	    }
 }
--- bo-gui/bo-gui-project/src/bo_gui/gui/MainWindow.java
+++ bo-gui/bo-gui-project/src/bo_gui/gui/MainWindow.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
@@ -24,9 +25,13 @@ import bo_gui.executor.SolutionStruct;
 
 public class MainWindow {
 	
-	protected JTextPane text_pane;
 	private JScrollPane scroll_pane;
+	
+	protected JTextPane text_pane;
 	protected Exectuor_Thread_Menager menager;
+	protected Area_MapArea map;
+	protected Area_GraphArea graph;
+	protected JButton StartButton;
 	
 	public MainWindow(){
 		
@@ -39,52 +44,69 @@ public class MainWindow {
 		JPanel main_panel = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.NONE;
+		/*
 		c.gridwidth = 2;
 		c.gridx = 0;
 		c.gridy = 0;
-		
-		JPanel top_panel = new JPanel();
-		main_panel.add(top_panel);
-		
+		*/
 		c.gridwidth = 1;
-		c.gridx = 1;
-		c.gridy = 1;
+		c.gridx = 0;
+		c.gridy = 0;
 		JPanelExtended left_panel = new JPanelExtended();
 		main_panel.add(left_panel);
 		
 		c.gridx = 1;
-		c.gridy = 1;
+		c.gridy = 0;
 		JPanelExtended right_panel = new JPanelExtended();
 		main_panel.add(right_panel);
 		
+		c.gridwidth = 2;
+		c.gridx = 0;
+		c.gridy = 1;
+		JPanel bottom_panel = new JPanel();
+		main_panel.add(bottom_panel);
 		
-		JButton StartButton = new JButton("Start!");
+		StartButton = new JButton("Start!");
 		right_panel.doloz(StartButton);
 		JButtonListener ActionListener = new JButtonListener();
 		StartButton.addActionListener(ActionListener);
 		
+		right_panel.lf();
+		JLabel text_pane_label = new JLabel("Console");
+		right_panel.doloz(text_pane_label);
+		
 		text_pane = new JTextPane();
 		scroll_pane = new JScrollPane(text_pane);
-		scroll_pane.setPreferredSize(new Dimension(500,300));
-		scroll_pane.setMinimumSize(new Dimension(500,300));
-		scroll_pane.setSize(500, 300);
+		scroll_pane.setPreferredSize(new Dimension(400,100));
+		scroll_pane.setMinimumSize(new Dimension(400,100));
+		scroll_pane.setSize(400, 100);
 		//left_panel.setMinimumSize(new Dimension(500,500));
 		text_pane.setCaretPosition(text_pane.getDocument().getLength());
-		left_panel.doloz(scroll_pane);
+
+		right_panel.lf();
+		right_panel.doloz(scroll_pane, true);
 		
 		
-		left_panel.lf();
-		
-		Area_MapArea map = new Area_MapArea(this);
+		map = new Area_MapArea(this);
 		map.setPreferredSize(new Dimension(500,300));
 		map.setMinimumSize(new Dimension(500,300));
 		map.setSize(new Dimension(500,300));
-		map.setToolTipText("Rysowanie");
+		map.setToolTipText("Mapka");
 		left_panel.doloz(map);
 		
-		frame.getContentPane().add(main_panel,BorderLayout.CENTER);
+		left_panel.lf();
+		
+		graph = new Area_GraphArea(this);
+		graph.setPreferredSize(new Dimension(500,300));
+		graph.setMinimumSize(new Dimension(500,300));
+		graph.setSize(new Dimension(500,300));
+		graph.setToolTipText("Wykres funkcji celu");
+		left_panel.doloz(graph);
+		graph.clear();
+		
+		frame.getContentPane().add(main_panel,BorderLayout.WEST);
 		frame.pack();
-		frame.setSize(900,600);
+		//frame.setSize(900,600);
 		frame.setResizable(false);
 		frame.setVisible(true);
 	}
@@ -96,11 +118,7 @@ public class MainWindow {
 		}
 		
 		public void actionPerformed(ActionEvent event) {
-			// TODO Auto-generated method stub
-
-			text_pane.setText("");
 			menager.start_thread();
-			//String linia = exec.getOutput();
 		}
 	}
 }
