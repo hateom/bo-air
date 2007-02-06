dd0e987c4e680ae6a4ecb17ca735aac148802b43
tomasz.huczek <tomasz.huczek@f45e810e-8225-0410-8430-f7d63d91aa8e>
2007-02-06 17:16:04 +0000

    Fresh changes :-)

--- processor/src/p_solution.cpp
+++ processor/src/p_solution.cpp
@@ -2,12 +2,18 @@
 #include "p_solution.h"
 #include "p_error.h"
 #include "p_output.h"
+#include "p_cost_mgr.h"
 
-pSolution::pSolution() : size(0), vec(NULL)
+pSolution::pSolution() : vec(NULL), size(0)
 {
 }
 
-pSolution::pSolution( pSolution * ptr )
+pSolution::pSolution( unsigned int siz ) : size(siz)
+{
+    vec = new int[size];
+}
+
+pSolution::pSolution( pSolution * ptr ) : vec(NULL), size(0)
 {
     if( ptr->size )
     {
@@ -42,13 +48,24 @@ void pSolution::release()
     }
 }
 
+void pSolution::init( int max_value )
+{
+    if( !size ) return;
+
+    for( size_t i=0; i<size; ++i )
+    {
+        vec[i] = rand()%(max_value+1);
+    }
+}
+
 bool pSolution::equals( pSolution * s )
 {
     P_ASSERT( s != NULL, "empty argument" );
+    P_ASSERT( vec != NULL, "empty solution" );
 
     if( s->size != size ) return( false );
 
-    for( int i=0; i<size; ++i )
+    for( unsigned int i=0; i<size; ++i )
     {
         if( vec[i] != s->vec[i] )
         {
@@ -57,5 +74,54 @@ bool pSolution::equals( pSolution * s )
     }
 
     return( true );
+}
+
+pSolution & pSolution::swap( int i, int j )
+{
+    int swp;
+
+    swp = vec[i];
+    vec[i] = vec[j];
+    vec[j] = swp;
+
+    return *this;
+}
+
+pSolution & pSolution::inc( int i, int j )
+{
+    if( vec[i] < pc::transmitter_type_count()-1 ) vec[i]++;
+    if( vec[j] < pc::transmitter_type_count()-1 ) vec[j]++;
+    return *this;
+}
+
+pSolution & pSolution::dec( int i, int j )
+{
+    if( vec[i] > 0 ) vec[i]--;
+    if( vec[j] > 0 ) vec[j]--;
+    return *this;
+}
+
+/*
+pSolution * pSolution::copy()
+{
+    return new pSolution( this );
+}
+*/
+
+pSolution & pSolution::operator=( pSolution & sol )
+{
+    if( size == 0 )
+    {
+        vec = new int[sol.size];
+    }
+    else if( size != sol.size )
+    {
+        delete [] vec;
+        vec = new int[sol.size];
+    }
+
+    memcpy( vec, sol.vec, sizeof(int)*size );
+
+    return( *this );
 }
 
--- processor/src/p_tabu_search.h
+++ processor/src/p_tabu_search.h
@@ -9,6 +9,18 @@ class pTabuSearch
 public:
     pTabuSearch();
     virtual ~pTabuSearch();
+
+    int  exec();
+    void free();
+
+    void create_tl( size_t siz );
+    int & short_list( size_t i, size_t j );
+    int & long_list( size_t i, size_t j );
+    void decrease_short_list();
+
+protected:
+    int * tabu_list;
+    size_t ssize;
 };
 
 #endif // __P_TABU_SEARCH_H__
